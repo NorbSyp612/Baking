d5e2941583fdd1b23cca222ae763613fc962e3d7
NorbSyp612 <46010621+NorbSyp612@users.noreply.github.com>
2019-03-21 15:23:15 -0400

    Started adapter

--- app/src/main/java/com/example/baking/Utils/RecipesAdapter.java
+++ app/src/main/java/com/example/baking/Utils/RecipesAdapter.java
@@ -1,4 +1,48 @@
 package com.example.baking.Utils;
 
-public class RecipesAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.baking.Items.Recipe;
+import com.example.baking.R;
+
+import java.util.ArrayList;
+import android.support.v7.widget.RecyclerView;
+
+public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.NumberViewHolder> {
+
+    private ListItemClickListener onClickListener;
+    private static int viewHolderCount;
+    private int numberMovies;
+    private ArrayList<Recipe> recipes;
+
+    public interface ListItemClickListener {
+        void onListItemClick(int clickedItemIndex);
+    }
+
+    public RecipesAdapter (ListItemClickListener onClick, ArrayList<Recipe> recipesArray) {
+        onClickListener = onClick;
+        this.recipes = recipesArray;
+    }
+
+    @NonNull
+    @Override
+    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int layoutID = R.layout.recipe_item;
+        LayoutInflater inflater LayoutInflater.from(context);
+        boolean shouldAttachtoParentImm = false;
+
+        View view = inflater.inflate(layoutID, viewGroup, shouldAttachtoParentImm);
+        NumberViewHolder viewHolder = new NumberViewHolder(view);
+
+        viewHolderCount++;
+        return viewHolder;
+    }
+
+
 }
+
+
