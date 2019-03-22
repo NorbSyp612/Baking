ee79a4e3a71247511eb9b32cd9c4727da84a3dff
NorbSyp612 <46010621+NorbSyp612@users.noreply.github.com>
2019-03-22 15:07:24 -0400

    Adapter for recipe view beginning to work

--- app/src/main/java/com/example/baking/RecipeActivity.java
+++ app/src/main/java/com/example/baking/RecipeActivity.java
@@ -1,23 +1,29 @@
 package com.example.baking;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
 import com.example.baking.Items.Recipe;
 import com.example.baking.Utils.JsonParser;
+import com.example.baking.Utils.RecipesAdapter;
 
 import java.util.ArrayList;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements RecipesAdapter.ListItemClickListener {
 
     private ArrayList<Recipe> mRecipes;
     private Recipe mRecipe;
+    private RecyclerView mRecylerView;
+    private RecipesAdapter mRecipeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class RecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe);
 
         Intent intent = getIntent();
+        Context context = getApplicationContext();
 
         JsonParser jsonParser = new JsonParser(getApplicationContext());
         mRecipes = jsonParser.parseJson();
@@ -34,7 +41,18 @@ public class RecipeActivity extends AppCompatActivity {
 
         mRecipe = mRecipes.get(pos);
 
+        mRecylerView = (RecyclerView) findViewById(R.id.recipe_RecyclerView2);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+        mRecylerView.setLayoutManager(layoutManager);
+        mRecipeAdapter = new RecipesAdapter(1, this, mRecipe);
+        mRecylerView.setAdapter(mRecipeAdapter);
+        mRecylerView.setHasFixedSize(true);
+
 
     }
 
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+
+    }
 }
--- app/src/main/java/com/example/baking/Utils/RecipesAdapter.java
+++ app/src/main/java/com/example/baking/Utils/RecipesAdapter.java
@@ -2,11 +2,13 @@ package com.example.baking.Utils;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.baking.Items.Recipe;
+import com.example.baking.Items.RecipeIngredients;
 import com.example.baking.R;
 
 import java.util.ArrayList;
@@ -20,7 +22,9 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.NumberVi
     private static int viewHolderCount;
     private int numRecipes;
     private ArrayList<Recipe> recipes;
+    private Recipe recipe;
     private Boolean fromMain;
+    private ArrayList<RecipeIngredients> mIngredients;
 
     public interface ListItemClickListener {
         void onListItemClick(int clickedItemIndex);
@@ -34,61 +38,80 @@ public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.NumberVi
         fromMain = true;
     }
 
-    public void setBooleanFromMain(Boolean fromWhere) {
-        fromMain = fromWhere;
+    public RecipesAdapter(int numHolders, ListItemClickListener onClick, Recipe viewRecipe) {
+        numRecipes = numHolders;
+        onClickListener = onClick;
+        recipe = viewRecipe;
+        mIngredients = recipe.getRecipeIngredients();
+        viewHolderCount = 0;
+        fromMain = false;
     }
 
 
     @Override
     public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         Context context = viewGroup.getContext();
-        int layoutID = R.layout.recipe_item;
-        LayoutInflater inflater = LayoutInflater.from(context);
-        boolean shouldAttachToParentImmediately = false;
+        int layoutID = 0;
 
-        View view = inflater.inflate(layoutID, viewGroup, shouldAttachToParentImmediately);
-        NumberViewHolder viewHolder = new NumberViewHolder(view);
+        if (fromMain) {
+            layoutID = R.layout.recipe_item;
+        } else {
+            layoutID = R.layout.recipe_ingredients;
+        }
+            LayoutInflater inflater = LayoutInflater.from(context);
+            boolean shouldAttachToParentImmediately = false;
 
-        viewHolderCount++;
-        return viewHolder;
-    }
+            View view = inflater.inflate(layoutID, viewGroup, shouldAttachToParentImmediately);
+            NumberViewHolder viewHolder = new NumberViewHolder(view);
 
-    @Override
-    public int getItemCount() {
-        return numRecipes;
-    }
-
-    @Override
-    public void onBindViewHolder(NumberViewHolder holder, int i) {
-        holder.bind(i);
-    }
-
-    class NumberViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-        TextView recipeItemview;
-
-        public NumberViewHolder(View itemView) {
-
-            super(itemView);
-
-            if (fromMain) {
-                recipeItemview = (TextView) itemView.findViewById(R.id.recipe_item);
-                itemView.setOnClickListener(this);
-            }
-
+            viewHolderCount++;
+            return viewHolder;
         }
 
         @Override
-        public void onClick(View v) {
-            int clickedPosition = getAdapterPosition();
-            onClickListener.onListItemClick(clickedPosition);
+        public int getItemCount () {
+            return numRecipes;
         }
 
-        void bind(int listIndex) {
-            Context context = itemView.getContext();
-            recipeItemview.setText(recipes.get(listIndex).getName());
+        @Override
+        public void onBindViewHolder (NumberViewHolder holder,int i){
+            holder.bind(i);
+        }
+
+        class NumberViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            TextView recipeItemview;
+            TextView recipeIngredients;
+
+            public NumberViewHolder(View itemView) {
+
+                super(itemView);
+
+                if (fromMain) {
+                    recipeItemview = (TextView) itemView.findViewById(R.id.recipe_item);
+                    itemView.setOnClickListener(this);
+                } else {
+                    Log.d("TEST", "initializing recipeingredients");
+                    recipeIngredients = (TextView) itemView.findViewById(R.id.recipe_ingreds);
+                }
+
+            }
+
+            @Override
+            public void onClick(View v) {
+                int clickedPosition = getAdapterPosition();
+                onClickListener.onListItemClick(clickedPosition);
+            }
+
+            void bind(int listIndex) {
+                if (fromMain) {
+                    recipeItemview.setText(recipes.get(listIndex).getName());
+                } else {
+                    String ing = mIngredients.get(0).getIngredient();
+                    recipeIngredients.setText(ing);
+                }
+            }
         }
     }
-}
 
 
